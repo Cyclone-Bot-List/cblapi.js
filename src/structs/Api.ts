4de07a255995b4b45de3c13b217601278976efac
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-04-20 22:40:12 +0500

    Update Api.ts

--- src/structs/Api.ts
+++ src/structs/Api.ts
@@ -1,50 +1,26 @@
-import fetch, { Headers } from "node-fetch";
-import ApiError from "../utils/ApiError";
-import qs from "querystring";
-import { EventEmitter } from "events";
+import fetch, { Headers } from 'node-fetch'
+import ApiError from '../utils/ApiError'
+import qs from 'querystring'
+import { EventEmitter } from 'events'
 
-import { SnowFlake, BotInfo, BotStats, UserInfo } from "../typings";
+import { BotStats, BotInfo, UserInfo } from '../typings'
 
 interface APIOptions {
-  token?: string;
+    token?: string
 }
 
 export class Api extends EventEmitter {
-  private options: APIOptions;
+    private options: APIOptions
 
-  constructor(token: string, options: APIOptions = {}) {
-    super();
-    this.options = {
-      token: token,
-      ...options,
-    };
-  }
-
-  private async _request(
-    method: string,
-    path: string,
-    body?: object
-  ): Promise<any> {
-    const headers = new Headers();
-    if (this.options.token) headers.set("Authorization", this.options.token);
-    if (method !== "GET") headers.set("Content-Type", "application/json");
-
-    let url = ``;
-
-    // @ts-ignore querystring typings are messed
-    if (body && method === "GET") url += `?${qs.stringify(body)}`;
-
-    const response = await fetch(url, {
-      method,
-      headers,
-      body: body && method !== "GET" ? JSON.stringify(body) : null,
-    });
-
-    let responseBody;
-    if (response.headers.get("Content-Type")?.startsWith("application/json")) {
-      responseBody = await response.json();
-    } else {
-      responseBody = await response.text();
+    constructor (token: string, options: APIOptions = {}) {
+        super()
+        this.options = {
+            token: token,
+            ...options
+        }
     }
-  }
+
+    private async _request (method: string, path: string, body?: object): Promise<any> {
+        const headers = new Headers()
+    }
 }
