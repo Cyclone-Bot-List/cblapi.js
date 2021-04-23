880973f1337edbe9d7dd31fd6d60173e4b03eccb
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-04-24 00:17:54 +0500

    Update Api.ts

--- src/structs/Api.ts
+++ src/structs/Api.ts
@@ -22,5 +22,31 @@ export class Api extends EventEmitter {
 
     private async _request (method: string, path: string, body?: object): Promise<any> {
         const headers = new Headers()
+        if (this.options.token) headers.set('Authorization', this.options.token)
+        if (method !== 'GET') headers.set('Content-Type', 'application/json')
+
+        let url = `https://cyclonebots.xyz/api/${path}`
+
+        //@ts-ignore
+        if (body && method == 'GET') url += `?${qs.stringify(body)}`
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body: body && method !== 'GET' ? JSON.stringify(body) : null
+        })
+
+        let responseBody
+        if (responseBody.headers.get('Content-Type')?.startsWith('application/json')) {
+            responseBody = await response.json()
+        } else {
+            responseBody = await response.text()
+        }
+
+        if (!response.ok) {
+            throw new ApiError(response.status, response.statusText, responseBody)
+        }
+
+        return responseBody
     }
 }
